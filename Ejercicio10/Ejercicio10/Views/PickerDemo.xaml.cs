8158c0d955c7e99eee3ff88280e85b636c514656
oswaltec <oswaltec@gmail.com>
2021-06-17 21:23:44 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio10/Ejercicio10/Views/PickerDemo.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickerDemo : ContentPage
+    {
+        public PickerDemo()
+        {
+            InitializeComponent();
+            this.BindingContext = new MonkeysPageViewModel();
+        }
+    }
+}
